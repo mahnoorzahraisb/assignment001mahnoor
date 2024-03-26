bd5effbfd75f9c395e2b9fe5dd0df0af9744e273
Mahnoor Zahra <mahnoor.zahra321@gmail.com>
2024-03-26 21:14:30 +0500

    first assignment initial commit

---
+++ index.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+
+const _prompt = await inquirer.prompt([
+    {
+        type: "number",
+        name: "firstNumber",
+        message: 'Enter the first number: ',
+    },
+    {
+        type: "number",
+        name: "secondNumber",
+        message: 'Enter the second number: ',
+    },
+    {
+        type: 'list',
+        name: 'operator',
+        message: 'Select the math operation you want to perform:',
+        choices: ['Add', 'Subtract', 'Multiply', 'Divide']
+    }
+]);
+
+let result:number;
+
+switch(_prompt.operator){
+    case 'Add':
+        result = _prompt.firstNumber + _prompt.secondNumber;
+        console.log("The prompt for addition is: " + result);
+        break;
+    
+    case 'Subtract':
+        result = _prompt.firstNumber - _prompt.secondNumber;
+        console.log("The prompt for subtraction is: " + result);
+        break;
+
+    case 'Multiply':
+        result = _prompt.firstNumber * _prompt.secondNumber;
+        console.log("The prompt for multiplication is: " + result);
+        break;
+
+    case 'Divide':
+        result = _prompt.firstNumber / _prompt.secondNumber;
+        console.log("The prompt for division is: " + result);
+        break;
+}
